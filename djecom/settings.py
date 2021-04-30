8dab919c58e5dba2c745d223b8caabad80d8799c
Joy Krishan <joykrishan10@gmail.com>
2021-04-30 21:20:40 +0600

    initial commit

--- djecom/settings.py
+++ djecom/settings.py
@@ -69,8 +69,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "AmazonJoy",
+        "USER": "postgres",
+        "PASSWORD": "joyjoyjoy"
     }
 }
 
