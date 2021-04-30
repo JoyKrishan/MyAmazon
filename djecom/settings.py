d236a961ee5ac1b818b004b7fe1b23a79aaa18b0
Joy Krishan <joykrishan10@gmail.com>
2021-04-30 20:17:39 +0600

    initial commit

--- djecom/settings.py
+++ djecom/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(
     os.path.dirname(os.path.abspath(__file__)))  # F:\1_DJANGO\ecomweb\ecom\src\django_project_boilerplate
 
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66cjazj'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
